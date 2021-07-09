0c5cc1a702b199e5c68103024865f279daca60ef
ivaquero <46377665+integzz@users.noreply.github.com>
2021-07-09 08:54:38 +0800

    fix

---
+++ Formula/powerlevel10k.rb
@@ -0,0 +1,26 @@
+class Powerlevel10k < Formula
+    desc "A Zsh Theme"
+    homepage "https://github.com/romkatv/powerlevel10k"
+    url "https://github.com/romkatv/powerlevel10k/archive/v1.15.0.tar.gz"
+    sha256 "3482ae9b876890b78dd2567066cf5ceecd37eb2117c12248106710655a7dc1dc"
+  
+    depends_on "zsh"
+  
+    def install
+      system 'make', 'pkg'
+      prefix.install Dir["*"]
+    end
+  
+    def caveats
+      <<~EOS
+        To activate powerlevel10k, add the following line to .zshrc:
+          source #{opt_prefix}/powerlevel10k.zsh-theme
+        If your .zshrc sets ZSH_THEME, remove that line.
+      EOS
+    end
+  
+    test do
+      assert_match "SUCCESS",
+        shell_output("zsh -fic '. #{opt_prefix}/powerlevel10k.zsh-theme && (( ${+P9K_SSH} )) && echo SUCCESS'")
+    end
+  end
