b0de86a9f5514bfaaf02e7071efa35a61f4995b6
ivaquero <46377665+integzz@users.noreply.github.com>
2021-09-24 19:20:00 +0800

    update files
    Signed-off-by: ivaquero <46377665+integzz@users.noreply.github.com>

--- Casks/ting-fr.rb
+++ Casks/ting-fr.rb
@@ -1,6 +1,6 @@
 cask "ting-fr" do
   version "9.2.0"
-  sha256 "cbb80154534d2b37995d178e51c82f73b8cd5374a8381d6b034bd505a6ae0e00"
+  sha256 "3b0b47870ab4650e4efd3ca83fa21bd10eb7917caff3c6040eac70b3847db26a"
 
   url "https://static.frdic.com/pkg/ting_fr/ting_fr.dmg",
       user_agent: :fake
